d55e3e63a67fe68b420cd9d6d00155a8676f9b88
Kubi Kubisiak <mckubisiak@gmail.com>
2021-09-09 10:16:04 -0700

    characterlist component completed and displaying

--- src/components/app/Characterlist.jsx
+++ src/components/app/Characterlist.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Character from './Character';
 
 const Characterlist = ({ characters }) => (
   <ul aria-label="characters">
     {characters.map((character) => (
-      <li key={character.id}></li>
+      <li key={character.id}>
+        <Character name={character.name} image={character.image} />
+      </li>
     ))}
   </ul>
 );
+
+Characterlist.propTypes = {
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      image: PropTypes.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Characterlist;
--- src/containers/HeyArnoldContainer.jsx
+++ src/containers/HeyArnoldContainer.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-../services/heyArnoldApi
+import Characterlist from '../components/app/Characterlist';
+import { fetchCharacters } from '../services/heyArnoldApi';
+
 export default class HeyArnoldContainer extends Component {
+  state = {
+    loading: true,
+    characters: [],
+  };
+
+  componentDidMount() {
+    fetchCharacters().then((characters) =>
+      this.setState({ loading: false, characters })
+    );
+  }
 
   render() {
-    
-    return <h3>HHHEEYYY!</h3>;
+    return <Characterlist characters={this.state.characters}/>;
   }
 }
